a3cf214aae81f97bae18f88106c59e25da5b4f0c
kirniy1 <kirniy@icloud.com>
2024-06-21 15:43:56 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "vnvnc-coin-tapper-server",
+  "version": "1.0.0",
+  "description": "Server for VNVNC Coin Tapper game",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "mongodb": "^4.1.0",
+    "dotenv": "^10.0.0",
+    "cors": "^2.8.5"
+  }
+}
